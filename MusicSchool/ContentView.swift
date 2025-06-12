264f9af470d8eea460cf738177964a127580bd03
VorpalElf <jeremy20131085@gmail.com>
2025-06-12 18:53:35 +0100

    Initial Commit

---
+++ MusicSchool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicSchool
+//
+//  Created by Jeremy Lo on 12/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicSchool/MusicSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSchoolApp.swift
+//  MusicSchool
+//
+//  Created by Jeremy Lo on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
